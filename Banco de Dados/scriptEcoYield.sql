e73daf73a4fb8ba1b21f73369975558326f68508
Isabela Rosa <isabela.lima@sptech.school>
2024-03-19 15:08:06 -0300

    atializando banco de dados corrigido

--- Banco de Dados/scriptEcoYield.sql
+++ Banco de Dados/scriptEcoYield.sql
@@ -1,55 +1,62 @@
-create database sprint1;
-use sprint1;
+create database ecoYield;
+use ecoYield;
 
 create table cadastroEmpresa (
     idEmpresa INT auto_increment,
-    nomeEmpresa VARCHAR(50) NOT NULL,
-    CNPJ CHAR(14) NOT NULL,
+    nomeEmpresa VARCHAR(50) not null,
+    CNPJ CHAR(14) not null,
     segmentoEmpresa VARCHAR(45),
-    CEP CHAR(8) NOT NULL,
-    numero char(4),
-    nomeUsuario VARCHAR(50) NOT NULL,
-    emailUsuario VARCHAR(45)NOT NULL,
-    senha VARCHAR(20) NOT NULL,
+    CEP CHAR(8) not null,
+    numeroLocal char(4),
+    nomeUsuario VARCHAR(50) not null,
+    emailUsuario VARCHAR(45) not null,
+    senha VARCHAR(20) not null,
     PRIMARY KEY (idEmpresa)
 );
 
--- precisa colocar um check ?
+-- não preciso do campo de confirmar senha, pois é desnecessário, afinal, o banco irá consultar a tabela login e o campo senha
 
 create table loginEmpresa (
-    nomeUsuario VARCHAR(50) NOT NULL,
-    emailUsuario VARCHAR(45)NOT NULL,
-    senha VARCHAR(20) NOT NULL,
-    dataHora datetime
+    idLogin INT auto_increment,
+    nomeUsuario VARCHAR(50) not null,
+    emailUsuario VARCHAR(45) not null,
+    senha VARCHAR(20) not null,
+    dataHora datetime,
+    PRIMARY KEY (idLogin)
+);
+
+create table suporte (
+    idOcorrencia int PRIMARY KEY auto_increment,
+    nomeUsuario VARCHAR(50) not null,
+    emailUsuario VARCHAR(45) not null,
+    telefoneUsuario CHAR(11) not null,
+    campoMensagem text not null
 );
 
 create table ambienteEmpresa (
-    idAmbiente int PRIMARY KEY auto_increment,
-    departamento VARCHAR(45),
+    idAmbiente int auto_increment,
+    descricao VARCHAR(45) DEFAULT 'Adicionar Descrição',
+    PRIMARY KEY (idAmbiente),
 );
-
-    -- alertaVerde 
-    -- alertaAmarelo
-    -- alertaVermelho
 
 create table sensor (
-    idSensor int PRIMARY KEY auto_increment,
-    qntDHT11 int,
-    qntLM35 int
+    idSensor int auto_increment,
+    tipo char (3) not null,
+    quantidade int not null,
+    alertaVerde int,
+    alertaAmarelo int,
+    alertaVermelho int,
+    CONSTRAINT check_tipo check (tipo('UMI','TEM'))
 );
 
-create table dadosSensor (
-    idDados int PRIMARY KEY auto_increment,
+create table leituraSensor (
+    idLeitura int PRIMARY KEY auto_increment,
     valorTemperaturaDht float,
     valorTemperaturaLm float,
     valorUmidadeDht float,
     dataHora datetime
 );
 
-create table suporte (
-    idOcorrencia int PRIMARY KEY auto_increment,
-    nomeUsuario VARCHAR(50) NOT NULL,
-    emailUsuario VARCHAR(45)NOT NULL,
-    telefoneUsuario CHAR(11) NOT NULL,
-    campoMensagem text NOT NULL
-);
+
+
+
